c59eb08cf07d499a1ab554c5a487299c03ef858e
nataliezd <zdroykowski.n@northeastern.edu>
2024-12-14 09:52:22 -0600

    Created board

--- app.js
+++ app.js
@@ -7,8 +7,20 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/MatchFlower1.png'
         },
         {
+            name: 'flower1',
+            img: 'images/MatchFlower1.png'
+        },
+        {
             name: 'flower2',
             img: 'images/MatchFlower2.png'
+        },
+        {
+            name: 'flower2',
+            img: 'images/MatchFlower2.png'
+        },
+        {
+            name: 'flower3',
+            img: 'images/MatchFlower3.png'
         },
         {
             name: 'flower3',
@@ -19,29 +31,41 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/MatchFlower4.png'
         },
         {
+            name: 'flower4',
+            img: 'images/MatchFlower4.png'
+        },
+        {
+            name: 'flower5',
+            img: 'images/MatchFlower5.png'
+        },
+        {
             name: 'flower5',
             img: 'images/MatchFlower5.png'
         },
         {
             name: 'flower6',
             img: 'images/MatchFlower6.png'
+        },
+        {
+            name: 'flower6',
+            img: 'images/MatchFlower6.png'
         }
     ]
-})  
+    
+    const grid = document.querySelector('.grid')
 
-const grid = document.querySelector('.grid')
+    // creating the board
 
-// creating the board
-
-function CreateBoard() {
-    for (let i = 0; i < cardArray.length; i++) {
-        var card = document.createElement('img')
-        card.setAttribute('src', 'images/dirt.png')
-        card.setAttribute('data-id', i)
-        // card.addEventListener('click', FlipCard)
-        grid.appendChild(card)
+    function CreateBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/MatchGrass.png')
+            card.setAttribute('data-id', i)
+            // card.addEventListener('click', FlipCard)
+            grid.appendChild(card)
+        }
     }
-}
 
-CreateBoard()
+    CreateBoard()
+})  
 
